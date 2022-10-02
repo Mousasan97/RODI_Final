f4390adef4df023ebf35f11a7e81a7c8dc2c5eba
Mousasan97 <mousa.sondoqah@mail.polimi.it>
2022-10-03 01:06:30 +0200

    first commit

---
+++ JAM/create_db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 16:33:34 2021
+
+@author: alessandroaustoni
+"""
+
+from psycopg2 import (connect)
+from werkzeug.security import  generate_password_hash
+
+cleanup = ('DROP TABLE IF EXISTS jam_user CASCADE')
+
+commands = (
+        """
+        CREATE TABLE jam_user (
+            user_id SERIAL PRIMARY KEY,
+            user_name VARCHAR(255) UNIQUE NOT NULL,
+            user_password VARCHAR(255) NOT NULL,
+            user_mail VARCHAR(255) UNIQUE NOT NULL,
+            admin INTEGER
+
+        )
+        """    
+        )
+
+sqlCommands = (
+        'INSERT INTO jam_user (user_name, user_password, user_mail, admin) VALUES (%s, %s, %s, %s) RETURNING user_id'
+        )       
+
+#conn = connect("host='localhost' port='5432' dbname='postgres' user='postgres' password='Alhamdulilah1_'")
+
+# conn = connect("host='localhost' port='5432' dbname='JAM_db' user='JAM' password='SWfire07'")
+conn = connect("host='localhost' port='5433' dbname='postgres' user='postgres' password='admin'")
+
+cur = conn.cursor()
+
+cur.execute(cleanup)
+
+cur.execute(commands)
+print('execute command')
+    
+cur.execute(sqlCommands, ('Giuseppe', generate_password_hash('3ety3e7'), 'giuseppe@aaa.com','0')) #admin=0 -> normal user | admin=1 -> specialized user | admin=2 -> Super User
+pw='Geoinfo2021'
+admin_pass=generate_password_hash(pw)
+cur.execute(sqlCommands, ('JAM', admin_pass, 'mrnm.jam.team@gmail.com','2')) 
+userId = cur.fetchone()[0]
+
+cur.close()
+
+conn.commit()
+conn.close()
---
+++ README.md
@@ -0,0 +1,28 @@
+<h1>RODI is ready for the action...</h1>
+
+![image](https://user-images.githubusercontent.com/82569748/120941993-20ecb300-c726-11eb-9268-66b0c4c2abeb.png)
+
+<h2>Main goals<h2>
+
+
+  - Improve the level of communication between authorities in charge of road maintenance and citizens
+
+  - Measure and display information about the Key Performance Indicators [KPIs] of the distresses’ requests of maintenance on the urban road network of Milano
+
+  - Generate a collaborative and public map of distresses in the urban road network of Milano
+
+
+  ![image](https://user-images.githubusercontent.com/82569748/120941988-16cab480-c726-11eb-9f7e-5307c72fe904.png)
+
+<h2>Hierarchical control access<h2>
+
+![image](https://user-images.githubusercontent.com/82569748/120942066-ab351700-c726-11eb-9dec-a563427dfe1c.png)
+
+<h2>Future improvements<h2>
+
+  - Pothole recognition - Computer vision
+  
+![image](https://user-images.githubusercontent.com/82569748/120942077-c011aa80-c726-11eb-9064-d3b00417a0a7.png)
+
+  - Dynamic Rendering
+  
